28babe8ec937880a2170a74b1007e5330595b217
Santiago <santimolirpo.28@gmail.com>
2021-08-21 16:39:23 -0500

    Add Entity

---
+++ todozulu.Common/Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace todozulu.Common.Models
+{
+    public class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+
+
+    }
+}
---
+++ todozulu.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace todozulu.Functions.Entities
+{
+    public class TodoEntity : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+    }
+}
